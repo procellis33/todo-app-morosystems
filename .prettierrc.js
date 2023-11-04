ef1e35b0a0c15530523fbb730f8c91fd7e012362
Romanov Oleksii <aleksey55121@gmail.com>
2023-11-04 17:15:50 +0100

    Webpack React TS ESLint Prettier template base

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // arrowParens: "avoid",
+  // bracketSameLine: true,
+  // bracketSpacing: false,
+  // tabWidth: 2,
+  // useTabs: false,
+
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+};
---
+++ src/client/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const rootEl = document.querySelector('#root');
+if (rootEl === null) {
+  throw new Error('Cannot find root element with that id');
+}
+const root = createRoot(rootEl);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
