a49ead3674b2faeff302a7a6e59385557f9e8a66
Romanov Oleksii <aleksey55121@gmail.com>
2023-11-04 17:38:37 +0100

    Update prettier config

---
+++ .prettierrc.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: false,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 80,
+};
